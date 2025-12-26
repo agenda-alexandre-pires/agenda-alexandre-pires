6a1b46670625dde425cce905e615a0c7af3d877d
agenda-alexandre-pires <porfamail@gmail.com>
2025-12-26 02:46:53 -0300

    Add files via upload
    Adicionando config docker e fotos

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Ensure "public" folder is used for static assets
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     sourcemap: false
+  },
+  server: {
+    host: true
   }
 });
